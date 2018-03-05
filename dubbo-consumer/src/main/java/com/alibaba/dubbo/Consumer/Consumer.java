e60fbc8c1631da50d8bd9882a12159077a42b680
GitHubzcc <471295986@qq.com>
2018-03-05 22:13:59 +0800

    fix 客户端和服务端包与链接问题

--- dubbo-consumer/src/main/java/com/alibaba/dubbo/Consumer/Consumer.java
+++ dubbo-consumer/src/main/java/com/alibaba/dubbo/Consumer/Consumer.java
@@ -7,10 +7,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Created by eriz on 2018/3/5.
  */
 public class Consumer {
+    //http://www.itmayun.com/it/jf/Resource/download/901920001882583 下载admin客户端
     public static void main(String[] args) {
         //测试常规服务
         ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("consumer.xml");
+                new ClassPathXmlApplicationContext("dubbo-consumer.xml");
         context.start();
         System.out.println("consumer start");
         DemoService demoService = context.getBean(DemoService.class);
