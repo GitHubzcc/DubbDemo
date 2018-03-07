53f6bca1a09e707d2f81cb3b2222cc290afac586
eriz <eriz@hboxs.com>
2018-03-07 11:08:51 +0800

    add consumer

--- dubbo-consumer/src/main/java/com/alibaba/dubbo/Consumer/Consumer.java
+++ dubbo-consumer/src/main/java/com/alibaba/dubbo/Consumer/Consumer.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Created by eriz on 2018/3/5.
  */
 public class Consumer {
-    //http://www.itmayun.com/it/jf/Resource/download/901920001882583 下载admin客户端
+
     public static void main(String[] args) {
         //测试常规服务
         ClassPathXmlApplicationContext context =
